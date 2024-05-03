c161f0d66052d75d7717b78bad43afd0855dac2b
isosel26000 <yassine.ainane@livecampus.tech>
2024-05-03 11:57:33 +0200

    add css for card

--- frontend/src/pages/pokemon/[name].js
+++ frontend/src/pages/pokemon/[name].js
@@ -1,31 +1,40 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router'; // Importez useRouter de next/router
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { getPokemonByName } from '../../app/services/pokemonApi';
+import typeColors from '../../app/styles/typeColors';
+import styles from '../../app/styles/PokemonDetails.module.css';
 
 export default function PokemonDetails() {
-    const router = useRouter(); // Utilisez useRouter au lieu de useParams
-    const { name } = router.query; // Accédez aux paramètres de l'URL avec router.query
+    const router = useRouter();
+    const { name } = router.query;
     const [pokemon, setPokemon] = useState(null);
 
     useEffect(() => {
-        if (name) { // Ajoutez une vérification pour s'assurer que le nom est défini avant de faire la requête
+        if (name) {
             getPokemonByName(name)
-                .then(data => setPokemon(data))
-                .catch(error => console.error('Erreur lors de la récupération du Pokémon:', error));
+                .then(data => {
+                setPokemon(data);
+            })
+            .catch(error => console.error('Erreur lors de la récupération du Pokémon:', error));
         }
     }, [name]);
 
+    if (!pokemon) return <p>Loading...</p>;
+
     return (
-        <div>
-            {pokemon && (
-                <div>
-                    <h2>{pokemon.name}</h2>
-                    {pokemon.stats.map((stat, index) => (
-                        <p key={index}>{stat.name}: {stat.base_stat}</p>
-                    ))}
-                    <p>Types: {pokemon.types.join(', ')}</p>
-                </div>
-            )}
+        <div className={styles.pokemonDetails}>
+            <div className={styles.hp}>HP: {pokemon.stats.find(stat => stat.name === 'hp').base_stat}</div>
+            <h2 className={styles.pokemonName}>{pokemon.name}</h2>
+            <div className={styles.type} style={{ backgroundColor: typeColors[pokemon.types[0]] }}>
+                {pokemon.types[0]}
+            </div>
+            <div className={styles.statsContainer}>
+                {pokemon.stats.filter(stat => stat.name !== 'hp').map((stat, index) => (
+                    <div key={index} className={styles.statItem}>
+                        {stat.name.replace('-', ' ')}: {stat.base_stat}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
